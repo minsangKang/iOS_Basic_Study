be8cec139a7f2fa5e857aa2353739cebe864261a
FreeDeveloper97 <freedeveloper97@gmail.com>
2023-10-08 12:45:05 +0900

    Feat: [Basic_CoreData] Model & Entity & Attribute 정의

---
+++ Basic_CoreData/Basic_CoreData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Basic_CoreData
+//
+//  Created by Kang Minsang on 2023/10/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
